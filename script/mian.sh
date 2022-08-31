726092f2afd3524fab71d36510cdc55c5248a79b
damengzhu <80020206+damengzhu@users.noreply.github.com>
2022-08-31 13:31:44 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -2,7 +2,7 @@
 
 # 下载规则
 curl -o i-1.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
-curl -o i-2.txt https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt
+curl -o i-2.txt http://msedl.360safe.com/360mse/bcdbgimoikiapdjnifgghhgcfoclpchk/100129/bcdbgimoikiapdjnifgghhgcfoclpchk.txt
 curl -o i-3.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad3.txt
 
 # 合并规则并去除重复项
@@ -17,7 +17,7 @@ num=`cat i-tmp.txt | wc -l`
 # 添加标题和时间
 echo "[Adblock Plus 2.0]" >> i-tpdate.txt
 echo "! Title: abpmerge Rules" >> i-tpdate.txt
-echo "! Description: 该规则合并自AdFilters，adgk，jiekouAD" >> i-tpdate.txt
+echo "! Description: 该规则合并自AdFilters，360，jiekouAD" >> i-tpdate.txt
 echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> i-tpdate.txt
 echo "! Total count: $num" >> i-tpdate.txt
 cat i-tpdate.txt i-tmp.txt > abpmerge.txt
