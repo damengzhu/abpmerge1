31ee910ff7aacec9cf318da37764b723dfa69777
damengzhu <80020206+damengzhu@users.noreply.github.com>
2022-10-11 17:53:38 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -9,7 +9,7 @@ curl -o i-4.txt https://filters.adtidy.org/android/filters/224_optimized.txt
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
 cat i-mergd.txt | grep -v '^!' | grep -v '^！' | grep -v '^# ' | grep -v '^# ' | grep -v '^\[' | grep -v '^\【' > i-tmpp.txt
-sort -n i-tmpp.txt | uniq > i-tmp.txt
+sort i-tmpp.txt | uniq > i-tmp.txt
 
 
 # 计算规则数
