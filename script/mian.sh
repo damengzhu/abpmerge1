52b5a5f6fc8c680414dd9dc6ac29f1b21bf7b83c
damengzhu <80020206+damengzhu@users.noreply.github.com>
2022-12-16 00:44:25 +0800

    Update mian.sh

--- script/mian.sh
+++ script/mian.sh
@@ -4,7 +4,7 @@
 curl -o i-1.txt https://raw.githubusercontent.com/damengzhu/banad/main/jiekouAD.txt
 curl -o i-2.txt https://filters.adtidy.org/android/filters/2_optimized.txt
 curl -o i-3.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad.txt
-curl -o i-4.txt https://raw.githubusercontent.com/o0HalfLife0o/list/master/ad-edentw.txt
+curl -o i-4.txt https://code.gitlink.org.cn/damengzhu/filelink/raw/branch/master/easylistnocssrule.txt
 
 # 合并规则并去除重复项
 cat i*.txt > i-mergd.txt
@@ -19,7 +19,7 @@ num=`cat i-tmp.txt | wc -l`
 # 添加标题和时间
 echo "[Adblock Plus 2.0]" >> i-tpdate.txt
 echo "! Title: abpmerge Rules" >> i-tpdate.txt
-echo "! Description: 该规则合并自jiekouAD，AdGuard中文语言规则，乘风视频广告过滤规则、EasylistChina、EasylistLite、CJX'sAnnoyance，以及补充的一些规则，Adblock Warning Removal List、ABP filters、anti-adblock-killer-filters。" >> i-tpdate.txt
+echo "! Description: 该规则合并自jiekouAD，AdGuard中文语言规则，easylistnocssrule，乘风视频广告过滤规则、EasylistChina、EasylistLite、CJX'sAnnoyance，以及补充的一些规则" >> i-tpdate.txt
 echo "! Version: `date +"%Y-%m-%d %H:%M:%S"`" >> i-tpdate.txt
 echo "! Total count: $num" >> i-tpdate.txt
 cat i-tpdate.txt i-tmp.txt > abpmerge.txt
